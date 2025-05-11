3522831f6cf07c08090de7eae666c0d8ba6a9ba1
Mohammed AlMakhmari <mohammed.almakhmari@saab.ae>
2025-05-11 15:23:14 +0400

    New Dotfiles

--- micro/plug/lintertypescript/lint.lua
+++
@@ -1,8 +0,0 @@
-VERSION = "1.0.3"
-
-local config = import("micro/config")
-
-function init()
-    linter.makeLinter("tsc", "typescript", "tsc", {"--noEmit", "--pretty", "false"}, "%f%(%l,%c%): %m")
-    config.AddRuntimeFile("lintertypescript", config.RTHelp, "help/linter-typescript.md")
-end
